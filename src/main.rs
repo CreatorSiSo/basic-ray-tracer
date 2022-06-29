1b90895ece8c8527c6a4940cf9a2efa20c3d82b8
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2022-06-29 20:36:34 +0200

    Change name to basic-ray-tracer.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 
 use glam::{vec3a, vec4};
 use hittable::{HitRecord, Hittable};
-use std::error::Error;
 use std::fs::File;
 use std::io::BufWriter;
 
@@ -12,15 +11,15 @@ mod ray;
 mod renderer;
 mod scene;
 use primitive::Primitive;
-use primitive::Sphere;
+use primitive::{Primitive, Sphere};
 use ray::Ray;
 use renderer::{CpuRenderer, Renderer};
 use scene::Scene;
 
-const WIDTH: u32 = 2000;
-const HEIGHT: u32 = 2000;
+const WIDTH: u32 = 128;
+const HEIGHT: u32 = 128;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), std::io::Error> {
 	let mut renderer = CpuRenderer::new(WIDTH as f32, HEIGHT as f32);
 	let mut scene = Scene::default();
 	scene.push(Primitive::Sphere(Sphere::new(vec3a(0., 0., -1.), 0.5)));
@@ -42,7 +41,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 	});
 
 	// Write final image to png file
-	let path = "/home/creatorsiso/dev/basic_ray_tracer/result.png";
+	let path = "/home/creatorsiso/dev/basic-ray-tracer/result.png";
 	let file = File::create(path)?;
 	let w = BufWriter::new(file);
 
